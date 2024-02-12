0fb3238741157e9b19dac582c2bcc35158805bd4
Tyrannanas <129948998+SimonBinzenbach@users.noreply.github.com>
2024-02-12 13:39:55 +0100

    added .gitignore and unreal blank c++ project

---
+++ UnrealWorkshop/Source/UnrealWorkshop/UnrealWorkshopGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealWorkshopGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALWORKSHOP_API AUnrealWorkshopGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
